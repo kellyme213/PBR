1adcb97ed40456b617605e422fb040e3febb2a63
Michael <kellyme213@gmail.com>
2020-05-18 00:30:39 -0400

    setup project

---
+++ PBR/PBR/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  PBR
+//
+//  Created by Michael Kelly on 5/18/20.
+//  Copyright © 2020 Michael Kelly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PBR
- 
+This project will hopefully be Physically Based GPU Bidirectional Path Tracing using Metal.
